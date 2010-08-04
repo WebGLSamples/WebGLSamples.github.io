99ccad8730f9130f5a3b5ef047a9ec14db983d06
Henrik Rydgard <hrydgard@google.com>
2010-08-04 12:20:57 +0200

    Morph demo initial commit.

---
+++ morph/morph_display.js
@@ -0,0 +1,309 @@
+// Copyright 2010 Google Inc. All Rights Reserved.
+//
+// TODO: This code uses a lot of undefined global variables so you currently
+// can't create more than one instance. Should fix that.
+
+function initWebGL(canvas) {
+  try {
+    gl = canvas.getContext("experimental-webgl");
+  }
+  catch(e) {
+  }
+  if (!gl) {
+    alert("Unable to initialize WebGL. Your browser may not support it.");
+    return null
+  }
+  return gl
+}
+
+function getShader(gl, id) {
+  var shaderScript = document.getElementById(id);
+  if (!shaderScript) {
+    return null;
+  }
+  var theSource = "";
+  var currentChild = shaderScript.firstChild;
+  while (currentChild) {
+    if (currentChild.nodeType == 3) {
+      theSource += currentChild.textContent;
+    }
+    currentChild = currentChild.nextSibling;
+  }
+  var shader;
+  if (shaderScript.type == "x-shader/x-fragment") {
+    shader = gl.createShader(gl.FRAGMENT_SHADER);
+  } else if (shaderScript.type == "x-shader/x-vertex") {
+    shader = gl.createShader(gl.VERTEX_SHADER);
+  } else {
+    return null;  // Unknown shader type
+  }
+  gl.shaderSource(shader, theSource);
+  gl.compileShader(shader);
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    alert("An error occurred compiling the shader (" + shaderScript.type
+          + "): " + gl.getShaderInfoLog(shader));
+    return null;
+  }
+  return shader;
+}
+
+function MorphDisplay(id, left_face, right_face, left_file, right_file) {
+  var morphPos = 0.5
+  var w = $(id).width()
+  var h = $(id).height()
+
+  var gl = null
+
+  var morphShaderProgram = null
+
+  var leftTexture = null
+  var rightTexture = null
+
+  var vertexCount = 0
+  var leftFace = left_face
+  var rightFace = right_face
+
+  var leftPosBuffer = null
+  var rightPosBuffer = null
+  var leftTCBuffer = null
+  var rightTCBuffer = null
+
+  var leftPosAttr = null
+  var rightPosAttr = null
+  var leftTCAttr = null
+  var rightTCAttr = null
+
+  this.sliderValue = function(value) {
+    morphPos = value
+    this.draw()
+  }
+
+  function initShaders() {
+    var fragmentShader = getShader(gl, "shader-fs");
+    var vertexShader = getShader(gl, "shader-vs");
+
+    morphShaderProgram = gl.createProgram();
+    gl.attachShader(morphShaderProgram, vertexShader);
+    gl.attachShader(morphShaderProgram, fragmentShader);
+    gl.linkProgram(morphShaderProgram);
+ 
+    if (!gl.getProgramParameter(morphShaderProgram, gl.LINK_STATUS)) {
+      alert("Unable to initialize the shader program.");
+    }
+    leftPosAttr = gl.getAttribLocation(morphShaderProgram, "aVertexPosition1");
+    rightPosAttr = gl.getAttribLocation(morphShaderProgram, "aVertexPosition2");
+    leftTCAttr = gl.getAttribLocation(morphShaderProgram,
+        "aTextureCoord1");
+    rightTCAttr = gl.getAttribLocation(morphShaderProgram,
+        "aTextureCoord2");
+    gl.enableVertexAttribArray(leftPosAttr);
+    gl.enableVertexAttribArray(rightPosAttr);
+    gl.enableVertexAttribArray(leftTCAttr);
+    gl.enableVertexAttribArray(rightTCAttr);
+  }
+
+  function texCoordsFromVertices(verts) {
+    var count = verts.length / 3
+    var tc = new Array(count * 2)
+    for (var i = 0; i < count; i++) {
+      tc[i * 2] = (verts[i * 3] + 1) / 2
+      tc[i * 2 + 1] = 1.0 - ((verts[i * 3 + 1] + 1) / 2)
+    }
+    return tc
+  }
+
+  function verticesFromFace(face) {
+    // First just copy over all the normal verts
+    var count = face.length
+    verts = new Array(count * 3 + 12)
+    for (var i = 0; i < count; i++) {
+      verts[i * 3 + 0] = face[i].x * 2 - 1
+      verts[i * 3 + 1] = (1 - face[i].y) * 2 - 1
+      verts[i * 3 + 2] = 0
+    }
+    // Then add the four corners at the end.
+    verts[face.length * 3 + 0] = -1
+    verts[face.length * 3 + 1] = 1
+    verts[face.length * 3 + 2] = 0
+    verts[face.length * 3 + 3] = 1
+    verts[face.length * 3 + 4] = 1
+    verts[face.length * 3 + 5] = 0
+    verts[face.length * 3 + 6] = 1
+    verts[face.length * 3 + 7] = -1
+    verts[face.length * 3 + 8] = 0
+    verts[face.length * 3 + 9] = -1
+    verts[face.length * 3 + 10] = -1
+    verts[face.length * 3 + 11] = 0
+    return verts
+  }
+
+  function initBuffers() {
+    var vertices1 = verticesFromFace(leftFace)
+    var vertices2 = verticesFromFace(rightFace)
+    var indices = [
+      0, 11, 1,
+      0, 12, 11,
+      0, 7, 12,
+      1, 11, 2,
+      2, 11, 3,
+      3, 11, 9,
+      3, 9, 4,
+      9, 10, 4,
+      10, 5, 4,
+      10, 6, 5,
+      10, 12, 6,
+      12, 6, 7,
+      8, 9, 10,
+      11, 8, 9,
+      12, 11, 8,
+      12, 10, 8,
+      13, 0, 1,
+      13, 1, 2,
+      16, 13, 2,
+      16, 2, 3,
+      16, 3, 4,
+      16, 4, 15,
+      15, 4, 5,
+      15, 5, 6,
+      14, 15, 6,
+      14, 6, 7,
+      14, 7, 0,
+      13, 14, 0
+    ]
+
+    vertexCount = indices.length
+
+    var texcoords1 = texCoordsFromVertices(vertices1);
+    var texcoords2 = texCoordsFromVertices(vertices2);
+
+    leftPosBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, leftPosBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new WebGLFloatArray(vertices1), gl.STATIC_DRAW);
+    rightPosBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, rightPosBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new WebGLFloatArray(vertices2), gl.STATIC_DRAW);
+    leftTCBuffer = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, leftTCBuffer)
+    gl.bufferData(gl.ARRAY_BUFFER, new WebGLFloatArray(texcoords1), gl.STATIC_DRAW)
+    rightTCBuffer = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, rightTCBuffer)
+    gl.bufferData(gl.ARRAY_BUFFER, new WebGLFloatArray(texcoords2), gl.STATIC_DRAW)
+    indexBuffer = gl.createBuffer()
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer)
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new
+        WebGLUnsignedShortArray(indices), gl.STATIC_DRAW)
+  }
+
+  function deinitBuffers() {
+    gl.bindBuffer(gl.ARRAY_BUFFER, null)
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null)
+    gl.deleteBuffer(leftPosBuffer)
+    gl.deleteBuffer(rightPosBuffer)
+    gl.deleteBuffer(leftTCBuffer)
+    gl.deleteBuffer(rightTCBuffer)
+    gl.deleteBuffer(indexBuffer)
+  }
+
+  function changed() {
+    deinitBuffers()
+    initBuffers()
+  }
+
+  function loadTexture(image_file, texture) {
+    var img = new Image()
+    img.onload = function() { handleTextureLoaded(img, texture) }
+    img.src = image_file
+    return texture
+  }
+
+  function handleTextureLoaded(image, texture) {
+    gl.bindTexture(gl.TEXTURE_2D, texture)
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image)
+    // choose a filter that doesn't require mips for NPOT
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    // must use clamp to edge for NPOT
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.bindTexture(gl.TEXTURE_2D, null)
+  }
+
+  function initTextures() {
+    leftTexture = gl.createTexture()
+    loadTexture(left_file, leftTexture)
+    rightTexture = gl.createTexture()
+    loadTexture(right_file, rightTexture)
+  }
+
+  this.setLeftFace = function(face, tex, crop) {
+    leftFace = face
+    loadTexture(tex, leftTexture)
+    deinitBuffers()
+    initBuffers()
+  }
+
+  this.setRightFace = function(face, tex, crop) {
+    rightFace = face
+    loadTexture(tex, rightTexture)
+    deinitBuffers()
+    initBuffers()
+  }
+
+  this.draw = function() {
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+    gl.useProgram(morphShaderProgram);
+
+    var perspectiveMatrix = makePerspective(45, w/h, 0.1, 100.0);
+    var mvMatrix = Matrix.I(4);
+    mvMatrix = mvMatrix.x(Matrix.Translation($V([-0.0, 0.0, -3.05])).ensure4x4());
+
+    var pUniform = gl.getUniformLocation(morphShaderProgram, "uPMatrix");
+    gl.uniformMatrix4fv(pUniform, false, new WebGLFloatArray(perspectiveMatrix.flatten()));
+    var mvUniform = gl.getUniformLocation(morphShaderProgram, "uMVMatrix");
+    gl.uniformMatrix4fv(mvUniform, false, new WebGLFloatArray(mvMatrix.flatten()));
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, leftPosBuffer);
+    gl.vertexAttribPointer(leftPosAttr, 3, gl.FLOAT, false, 0, 0);
+    gl.bindBuffer(gl.ARRAY_BUFFER, rightPosBuffer);
+    gl.vertexAttribPointer(rightPosAttr, 3, gl.FLOAT, false, 0, 0);
+    gl.bindBuffer(gl.ARRAY_BUFFER, leftTCBuffer);
+    gl.vertexAttribPointer(leftTCAttr, 2, gl.FLOAT, false, 0, 0);
+    gl.bindBuffer(gl.ARRAY_BUFFER, rightTCBuffer);
+    gl.vertexAttribPointer(rightTCAttr, 2, gl.FLOAT, false, 0, 0);
+
+    gl.activeTexture(gl.TEXTURE0)
+    gl.bindTexture(gl.TEXTURE_2D, leftTexture)
+    gl.activeTexture(gl.TEXTURE1)
+    gl.bindTexture(gl.TEXTURE_2D, rightTexture)
+
+    gl.uniform1i(gl.getUniformLocation(morphShaderProgram, "uSampler1"), 0);
+    gl.uniform1i(gl.getUniformLocation(morphShaderProgram, "uSampler2"), 1);
+    gl.uniform1f(gl.getUniformLocation(morphShaderProgram, "fade"), morphPos);
+    gl.uniform1f(gl.getUniformLocation(morphShaderProgram, "morph"), morphPos);
+
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.drawElements(gl.TRIANGLES, vertexCount, gl.UNSIGNED_SHORT, 0);
+  }
+
+  function autoDraw(morphdisplay) {
+    morphdisplay.draw()
+    setTimeout(function(){autoDraw(morphdisplay)}, 500)
+  }
+
+  gl = initWebGL($(id)[0])
+  if (!gl) {
+    alert("WebGL fail")
+    return
+  }
+
+  initShaders()
+  initBuffers()
+  initTextures()
+
+  gl.clearColor(0, 0, 0, 1)
+  gl.clearDepth(1.0)
+  this.draw()
+  var md = this
+  setTimeout(function(){autoDraw(md)}, 500)
+}
+
---
+++ morph/morph_editor.js
@@ -0,0 +1,258 @@
+// Copyright 2010 Google Inc. All Rights Reserved.
+
+nodeNames = [
+  "head",     // 0
+  "head L",   // 1
+  "L ear ",   // 2
+  "chin L",   // 3
+  "chin",     // 4
+  "chin R",   // 5
+  "R ear",    // 6
+  "head R",   // 7
+  "nose",     // 8
+  "mouth L",  // 9
+  "mouth R",  // 10
+  "eye L",    // 11
+  "eye R"     // 12
+]
+ 
+lines = [
+  {s: 0, e: 1},
+  {s: 1, e: 2},
+  {s: 2, e: 3},
+  {s: 3, e: 4},
+  {s: 4, e: 5},
+  {s: 5, e: 6},
+  {s: 6, e: 7},
+  {s: 7, e: 0},
+  {s: 9, e: 10},
+]
+
+// Util functions
+function circle(ctx,x,y,r) {
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, Math.PI*2, true);
+  ctx.closePath();
+  ctx.fill();
+}
+
+function circlestroke(ctx,x,y,r) {
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, Math.PI*2, true);
+  ctx.closePath();
+  ctx.stroke();
+}
+
+function rect(ctx,x,y,w,h) {
+  ctx.beginPath();
+  ctx.rect(x, y, w, h);
+  ctx.closePath();
+  ctx.fill();
+}
+
+function line(ctx,x1,y1,x2,y2) {
+  ctx.beginPath()
+  ctx.moveTo(x1,y1)
+  ctx.lineTo(x2,y2)
+  ctx.closePath()
+  ctx.stroke()
+}
+
+function distance(a, b) {
+  dx = a.x - b.x
+  dy = a.y - b.y
+  return Math.sqrt(dx*dx+dy*dy)
+}
+
+function formatFace(f) {
+  str = "<code>[<br>"
+  for (i in f) {
+    str += "&nbsp;&nbsp;{x:" + f[i].x.toFixed(3) + ", y:" + f[i].y.toFixed(3) + "},<br>"
+  }
+  str += "]</code>"
+  return str
+}
+
+morphPos = 0.0
+
+w = 300
+h = 380
+
+function MorphEditor(id, json_id, start_face, img_file_name) {
+  var ctx = $(id)[0].getContext("2d")
+  var leftLeft = $(id).offset().left;
+  var leftTop = $(id).offset().top;
+  var selected = -1
+  var dragging = false
+  var image = new Image()
+  var face = start_face
+
+  function mouseCoord(ev) {
+    return {
+      x: ev.pageX - leftLeft,
+      y: ev.pageY - leftTop
+    }
+  }
+
+  function draw() {
+    ctx.drawImage(image, 0, 0)
+
+    ctx.fillStyle = "rgba(255, 255, 255, 1.0)"
+    ctx.strokeStyle = "rgba(255, 255, 255, 1.0)"
+    ctx.lineWidth = 3
+    for (l in lines) {
+      start = face[lines[l].s]
+      end = face[lines[l].e]
+      line(ctx, start.x * w, start.y * h, end.x * w, end.y * h)
+    }
+
+    ctx.lineWidth = 1.5
+    for (p in face) {
+      circle(ctx, face[p].x * w, face[p].y * h, selected == p ? 8 : 6)
+    }
+
+    ctx.fillStyle = "rgba(0,0,0,1.0)"
+    ctx.strokeStyle = "rgba(0,0,0,1.0)"
+    for (l in lines) {
+      start = face[lines[l].s]
+      end = face[lines[l].e]
+      line(ctx, start.x * w, start.y * h, end.x * w, end.y * h)
+    }
+    ctx.font = "bold 12px sans-serif"
+    ctx.textBaseline = "top"
+    ctx.textAlign = "center"
+    for (p in face) {
+      ctx.fillStyle = selected == p ? "rgba(36,200,118,1.0)" : "rgba(0,0,0,1.0)"
+      var x = face[p].x * w
+      var y = face[p].y * h
+      circle(ctx, x, y, 5)
+      ctx.fillStyle = selected == p ? "rgba(36,200,118,1.0)" : "rgba(255,255,255,1.0)"
+      ctx.fillText(nodeNames[p], x + 1, y + 5)
+      ctx.fillStyle = "rgba(0,0,0,1.0)"
+      ctx.fillText(nodeNames[p], x, y + 4)
+    }
+  }
+
+  function setImage(file_name) {
+    image.onload = draw
+    image.src = file_name
+  }
+
+  function setFace(new_face) {
+    face = new_face
+  }
+
+  $(id).mousedown(function(ev) {
+    pos = mouseCoord(ev)
+    pos.x = pos.x / w
+    pos.y = pos.y / h
+    for (p in face) {
+      if (distance(face[p], pos) < 0.03) {
+        selected = p
+        draw()
+        dragging = true
+        return
+      }
+    }
+    selected = -1
+    dragging = false
+    return false  // prevent mouse selection
+  });
+
+  $(id).mousemove(function(ev) {
+    pos = mouseCoord(ev)
+    if (dragging && selected != -1) {
+      pos.x /= w
+      pos.y /= h
+      face[selected].x = pos.x
+      face[selected].y = pos.y
+    }
+    draw()
+  });
+
+  $(id).mouseup(function(ev) {
+    dragging = false
+    draw()
+    // Uncomment to grab code for edited face
+    // $(json_id).html(formatFace(face))
+  });
+
+  this.draw = draw
+  this.setImage = setImage
+  this.setFace = setFace
+
+  image.onload = draw
+  image.src = img_file_name
+}
+
+function Slider(id, notify) {
+  var ctx = $(id)[0].getContext("2d")
+  var w = $(id).width()
+  var h = $(id).height()
+  var dragging = false
+  var morphPos = 0.5
+  var draggable = true
+
+  function draw() {
+    var thumb_size = 20
+    var x = morphPos * (w - thumb_size) + thumb_size / 2
+    ctx.fillStyle = "#FFFFFF"
+    rect(ctx, 0, 0, w, h)
+
+    ctx.strokeStyle = "rgba(0,0,0,1.0)"
+    ctx.lineWidth = 2
+    line(ctx, thumb_size/2, h/2, w-thumb_size/2, h/2)
+
+    ctx.fillStyle = draggable ? "rgba(0,255,0,1.0)" : "rgba(150,150,150,1.0)"
+    circle(ctx, x, h/2, thumb_size / 2 - 2)
+    circlestroke(ctx, x, h/2, thumb_size / 2 - 2)
+  }
+
+  function setPos(pos) {
+    morphPos = pos
+    notify.sliderValue(morphPos)
+    draw()
+  }
+  function setDraggable(d) {
+    draggable = d
+    if (!draggable) {
+      dragging = false
+    }
+    draw()
+  }
+  this.draw = draw
+  this.setPos = setPos
+  this.setDraggable = setDraggable
+
+  $(id).mousedown(function(ev) {
+    if (!draggable) return false;
+    var leftLeft = $(id).offset().left;
+    dragging = true
+    morphPos = (ev.pageX - leftLeft) * 1.0 / w
+    if (notify != null) {
+      notify.sliderValue(morphPos)
+    }
+    draw()
+    return false  // prevent mouse selection
+  })
+  $(id).mousemove(function(ev) {
+    if (!draggable) return;
+    if (dragging) {
+      var leftLeft = $(id).offset().left;
+      morphPos = (ev.pageX - leftLeft) * 1.0 / w
+      if (morphPos < 0) morphPos = 0
+      if (morphPos > 1) morphPos = 1
+      if (notify != null) {
+        notify.sliderValue(morphPos)
+      }
+      draw()
+    }
+  })
+  $(id).mouseup(function(ev) {
+    dragging = false
+    draw()
+  })
+
+  draw()
+}
+
