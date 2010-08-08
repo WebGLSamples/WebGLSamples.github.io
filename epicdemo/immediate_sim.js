7acd22ad21f44aeb0e13eaf0aa414768910b49aa
Henrik Rydgard <hrydgard@google.com>
2010-08-09 00:31:55 +0200

    wip marching cubes (a bit buggy)

--- epicdemo/immediate_sim.js
+++ epicdemo/immediate_sim.js
@@ -23,31 +23,36 @@ function ImmSim() {
   var hasPos, hasNormal, hasUV
 
   var maxCount = 1024
-
+  var program = null
+  
   var posArray    = new Float32Array(maxCount * 3)
   var normalArray = new Float32Array(maxCount * 3)
   var uvArray     = new Float32Array(maxCount * 2)
 
-  this.begin = function(primitive) {
+  this.begin = function(primitive, p_program) {
     prim      = primitive
+    program   = p_program
     count     = 0
     hasPos    = false
     hasNormal = false
     hasUV     = false
   }
 
-  this.end = function(program) {
+  this.end = function() {
     if (count == 0)
       return;
-    this.draw(program)
+    for (var i = count * 3; i < posArray.length; i++)
+      posArray[i] = 0.0;
+    this.draw()
   }
 
   // This should not be called from outside the class.
-  this.draw = function(program) {
+  this.draw = function() {
     // First, copy our logged data into stream buffers.
     if (hasPos) {
       gl.bindBuffer(gl.ARRAY_BUFFER, posBuf)
       gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(posArray, 0, count * 3), gl.DYNAMIC_DRAW)
+      // gl.bufferData(gl.ARRAY_BUFFER, posArray, gl.DYNAMIC_DRAW)
       gl.enableVertexAttribArray(program.attribLoc["position"])
       gl.vertexAttribPointer(program.attribLoc["position"], 3, gl.FLOAT, false, 0, 0);
     }
@@ -58,7 +63,7 @@ function ImmSim() {
       gl.vertexAttribPointer(program.attribLoc["normal"], 3, gl.FLOAT, false, 0, 0);
     }
     if (hasUV) {
-      gl.bindBuffer(gl.ARRAY_BUFFER, posBuf)
+      gl.bindBuffer(gl.ARRAY_BUFFER, uvBuf)
       gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(uvArray, 0, count * 2), gl.DYNAMIC_DRAW)
       gl.enableVertexAttribArray(program.attribLoc["texCoord"])
       gl.vertexAttribPointer(program.attribLoc["texCoord"], 2, gl.FLOAT, false, 0, 0);
@@ -68,21 +73,21 @@ function ImmSim() {
   }
 
   this.pos = function(x, y, z) {
-    posArray[count * 3] = x
+    posArray[count * 3 + 0] = x
     posArray[count * 3 + 1] = y
     posArray[count * 3 + 2] = z
     hasPos = true
   }
 
   this.posv = function(vec) {
-    posArray[count * 3]     = vec[0]
+    posArray[count * 3 + 0] = vec[0]
     posArray[count * 3 + 1] = vec[1]
     posArray[count * 3 + 2] = vec[2]
     hasPos = true
   }
 
   this.normal = function(x, y, z) {
-    normalArray[count * 3] = x
+    normalArray[count * 3]     = x
     normalArray[count * 3 + 1] = y
     normalArray[count * 3 + 2] = z
     hasNormal = true
@@ -96,13 +101,13 @@ function ImmSim() {
   }
 
   this.uv = function(u, v) {
-    uvArray[count * 2] = u
+    uvArray[count * 2]     = u
     uvArray[count * 2 + 1] = v
     hasUV = true
   }
 
   this.uvv = function(vec) {
-    uvArray[count * 2] = vec[0] 
+    uvArray[count * 2]     = vec[0] 
     uvArray[count * 2 + 1] = vec[1]
     hasUV = true
   }
---
+++ epicdemo/marching_cubes.js
@@ -0,0 +1,205 @@
+// Marching cubes in Javascript
+//
+// Extremely badly and suboptimally implemented.
+// 
+// Yes, this is madness. But this should test those JS engines!
+//
+// Converted from the standard C implementation that's all over the web.
+
+function VertexInterp(isolevel,p1,p2,valp1,valp2, i) {
+  
+  if (Math.abs(isolevel-valp1) < 0.00001) return p1
+  if (Math.abs(isolevel-valp2) < 0.00001) return p2
+  if (Math.abs(valp1-valp2)    < 0.00001) return p1
+  
+  var mu = (isolevel - valp1) / (valp2 - valp1);
+  var pout = new Float32Array(3)
+  pout[0] = p1[0] + mu * (p2[0] - p1[0]);
+  pout[1] = p1[1] + mu * (p2[1] - p1[1]);
+  pout[2] = p1[2] + mu * (p2[2] - p1[2]);
+  /*
+  if (isNaN(pout[0])) alert("x" + p1[0] + " " + p2[0] + ": " + i)
+  if (isNaN(pout[1])) alert("y" + p1[1] + " " + p2[1] + ": " + i)
+  if (isNaN(pout[2])) alert("z" + p1[2] + " " + p2[2] + ": " + i)
+  */
+  return pout
+}
+
+// Returns total number of triangles. Fills triangles.
+function polygonize(grid, isolevel, verts) {
+  // Determine the index into the edge table which
+  // tells us which vertices are inside of the surface
+  var cubeindex = 0;
+  if (grid.val[0] < isolevel) cubeindex |= 1;
+  if (grid.val[1] < isolevel) cubeindex |= 2;
+  if (grid.val[2] < isolevel) cubeindex |= 4;
+  if (grid.val[3] < isolevel) cubeindex |= 8;
+  if (grid.val[4] < isolevel) cubeindex |= 16;
+  if (grid.val[5] < isolevel) cubeindex |= 32;
+  if (grid.val[6] < isolevel) cubeindex |= 64;
+  if (grid.val[7] < isolevel) cubeindex |= 128;
+  
+  var bits = edgeTable[cubeindex]
+  
+  // If cube is entirely in/out of the surface - bail, nothing to draw.
+  if (bits == 0) return 0;
+  
+  var vertlist = []
+  
+  // Find the vertices where the surface intersects the cube
+  if (bits & 1)    vertlist[0]  = VertexInterp(isolevel, grid.p[0], grid.p[1], grid.val[0], grid.val[1], 0);
+  if (bits & 2)    vertlist[1]  = VertexInterp(isolevel, grid.p[1], grid.p[2], grid.val[1], grid.val[2], 1);
+  if (bits & 4)    vertlist[2]  = VertexInterp(isolevel, grid.p[2], grid.p[3], grid.val[2], grid.val[3], 2);
+  if (bits & 8)    vertlist[3]  = VertexInterp(isolevel, grid.p[3], grid.p[0], grid.val[3], grid.val[0], 3);
+  if (bits & 16)   vertlist[4]  = VertexInterp(isolevel, grid.p[4], grid.p[5], grid.val[4], grid.val[5], 4);
+  if (bits & 32)   vertlist[5]  = VertexInterp(isolevel, grid.p[5], grid.p[6], grid.val[5], grid.val[6], 5);
+  if (bits & 64)   vertlist[6]  = VertexInterp(isolevel, grid.p[6], grid.p[7], grid.val[6], grid.val[7], 6);
+  if (bits & 128)  vertlist[7]  = VertexInterp(isolevel, grid.p[7], grid.p[4], grid.val[7], grid.val[4], 7);
+  if (bits & 256)  vertlist[8]  = VertexInterp(isolevel, grid.p[0], grid.p[4], grid.val[0], grid.val[4], 8);
+  if (bits & 512)  vertlist[9]  = VertexInterp(isolevel, grid.p[1], grid.p[5], grid.val[1], grid.val[5], 9);
+  if (bits & 1024) vertlist[10] = VertexInterp(isolevel, grid.p[2], grid.p[6], grid.val[2], grid.val[6], 10);
+  if (bits & 2048) vertlist[11] = VertexInterp(isolevel, grid.p[3], grid.p[7], grid.val[3], grid.val[7], 11);
+
+  // Change cubeindex into an offset into triTable.
+  cubeindex <<= 4
+  
+  var ntriang = 0;
+  var i = 0;
+  while (triTable[cubeindex + i] != -1) {
+    verts[i + 0] = vertlist[triTable[cubeindex + i + 0]];
+    verts[i + 1] = vertlist[triTable[cubeindex + i + 1]];
+    verts[i + 2] = vertlist[triTable[cubeindex + i + 2]];
+    i += 3;
+    ntriang++;
+  }
+  return ntriang;
+}
+
+// Basic sample effect that actually draws something.
+// Good starting point for new effects.
+function MarchingCubesEffect() {
+  var arrays = tdl.primitives.createCube(1.0)
+  var program = createProgramFromTags("spinning_cube_vs", "spinning_cube_fs")
+  var textures = []
+
+  var proj = new Float32Array(16)
+  var view = new Float32Array(16)
+  var world = new Float32Array(16)
+
+  var viewproj = new Float32Array(16)
+  var worldviewproj = new Float32Array(16)
+
+  var model = new tdl.models.Model(program, arrays, textures);
+
+  var eyePosition = new Float32Array([0, 0, 4])
+  var target = new Float32Array([0, 0, 0])
+
+  // Marching cubes data
+  var size = 8
+  var field = new Float32Array(size * size * size)
+
+  var m4 = tdl.fast.matrix4
+
+  m4.perspective(proj, tdl.math.degToRad(60), aspect, 0.1, 500);
+  m4.lookAt(view, eyePosition, target, up);
+
+  this.render = function(framebuffer, time, global_time) {
+    m4.rotationY(world, time)
+    m4.translate(world, [0, 0*Math.sin(time)*0.5, 0])
+    m4.mul(viewproj, view, proj)
+    m4.mul(worldviewproj, world, viewproj)
+    
+    gl.clearColor(0.5,0.5,0.5,1)
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+    gl.enable(gl.DEPTH_TEST)
+    gl.disable(gl.CULL_FACE)
+
+    var uniformsConst = {
+      u_worldviewproj: worldviewproj
+    }
+    var uniformsPer = {
+      u_worldviewproj: worldviewproj
+    }
+
+    model.drawPrep(uniformsConst)
+    
+    for (var i = 0; i < size * size * size; i++) {
+      field[i] = 0.0
+    }
+    
+    // Fill the field
+    for (var i = 0; i < 2; i++) {
+      for (var z = 0; z < size; z++) {
+        var z_offset = size * size * z;
+        var fz = (z - (size/2)) / (size/2)
+        for (var y = 0; y < size; y++) {
+          var y_offset = z_offset + size * y;
+          var fy = (y - (size/2)) / (size/2)
+          for (var x = 0; x < size; x++) {
+            var fx = (x - (size/2)) / (size/2)
+            if (i == 1) fx -= Math.sin(time) * 2;
+            var ffy = fy
+            
+            var val = 1.0/(0.15 + fx * fx + ffy * ffy + fz * fz) * 0.1
+            field[y_offset + x] += val
+          }
+        }
+      }
+    }
+
+    var isolevel = 0.3// + Math.sin(time) * 0.04
+    
+    imm.begin(gl.TRIANGLES, program)
+    var d = 1.0 / (size / 2);
+    var yd = size
+    var zd = size * size
+    
+    for (var z = 0; z < size - 1; z++) {
+      var z_offset = size * size * z;
+      var fz = (z - (size/2)) / (size/2) //+ 1
+      for (var y = 0; y < size - 1; y++) {
+        var y_offset = z_offset + size * y;
+        var fy = (y - (size/2)) / (size/2) //+ 1
+        for (var x = 0; x < size - 1; x++) {
+          var fx = (x - (size/2)) / (size/2) //+ 1
+          var q = y_offset + x
+          var grid = { p: [ [fx,   fy,   fz  ],
+                            [fx+d, fy,   fz  ],
+                            [fx,   fy+d, fz  ],
+                            [fx+d, fy+d, fz  ],
+                            [fx,   fy,   fz+d],
+                            [fx+d, fy,   fz+d],
+                            [fx,   fy+d, fz+d],
+                            [fx+d, fy+d, fz+d]
+                          ],
+                   val: [ field[q          ],
+                          field[q+1        ],
+                          field[q+   yd    ],
+                          field[q+1+ yd    ],
+                          field[q+       zd],
+                          field[q+1+     zd],
+                          field[q+   yd+ zd],
+                          field[q+1+ yd+ zd]
+                        ]
+                 }
+          var verts = []
+          var numtris = polygonize(grid, isolevel, verts)
+          for (var i = 0; i < numtris; i++) {
+            imm.posv(verts[i * 3 + 0]); imm.next()
+            imm.posv(verts[i * 3 + 1]); imm.next()
+            imm.posv(verts[i * 3 + 2]); imm.next()
+          }
+        }
+      }
+    }
+    
+    // window.console.log("Tri cnt: " + tricount)
+    // Draw a simple triangle sticking out of the cube, to show off the immediate mode simulator.
+    for (var i = -100; i < 100; i++) {
+     // imm.posv([0, 0, i]); imm.next();
+     // imm.posv([1, 1, i]); imm.next();
+     // imm.posv([1, 0, i]); imm.next();
+    }
+    imm.end()
+  }
+}
