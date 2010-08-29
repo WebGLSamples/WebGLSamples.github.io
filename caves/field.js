e3d5459a2db1c38af153cdf15f56031742ea6cbb
jas <devnull@localhost>
2010-08-29 21:50:21 +0200

    starting to define new field interface

--- caves/field.js
+++ caves/field.js
@@ -7,10 +7,17 @@
 field = {}
 
 
+field.NodeState = {
+    UNIFORM: 0,  // All voxels have the same value: node.value.
+    SUBDIVIDED: 1,  // Node has children. Descend into them to get values.
+    LEAF: 2  // This is a leaf node. Look at node.field array.
+};
+
 
 // A FieldNode is a cube of field space.
 // If not subdivided, this.value represents the value within the whole cube.
-field.FieldNode = function(minX, minY, minZ, size) {
+// size and blockSize must be powers of two.
+field.FieldNode = function(minX, minY, minZ, size, blockSize) {
   this.minX = minX;
   this.minY = minY;
   this.minZ = minZ;
@@ -20,27 +27,32 @@ field.FieldNode = function(minX, minY, minZ, size) {
   this.midY = this.minY + this.size * 0.5; 
   this.midZ = this.minZ + this.size * 0.5; 
 
+  this.blockSize = blockSize;
   this.value = 1.0;
-  this.subdivided = false;
+  this.state = field.NodeState.UNIFORM;
   this.children = [];
 };
 
-field.FieldNode.prototype.whichChild(x, y, z) = function() {
+field.FieldNode.prototype.whichChild = function(x, y, z) {
   return 1 * (x >= this.midX) |
          2 * (y >= this.midY) |
          4 * (z >= this.midZ);
 }
 
-field.FieldNode.prototype.getValue(x, y, z) = function() {
-  if (this.subdivided) {
+field.FieldNode.prototype.getValue = function(x, y, z) {
+  if (this.state === field.NodeState.UNIFORM) {
+    return this.value;
+  } else if (this.state === field.NodeState.SUBDIVIDED) {
     var child = this.whichChild(x, y, z);
     return this.children[child].getValue(x, y, z);
   } else {
-    return this.value;
+    // state === field.NodeState.LEAF
+    // TODO
   }
 }
 
-field.FieldNode.prototype.subdivide() = function() {
+field.FieldNode.prototype.subdivide = function() {
+  // No bounds/size checking performed!
   var halfSize = size / 2;
   this.children[0] = new field.FieldNode(this.minX, this.minY, this.minZ, halfSize);
   this.children[1] = new field.FieldNode(this.minX + halfSize, this.minY, this.minZ, halfSize);
@@ -50,9 +62,20 @@ field.FieldNode.prototype.subdivide() = function() {
   this.children[5] = new field.FieldNode(this.minX + halfSize, this.minY, this.minZ + halfSize, halfSize);
   this.children[6] = new field.FieldNode(this.minX, this.minY + halfSize, this.minZ + halfSize, halfSize);
   this.children[7] = new field.FieldNode(this.minX + halfSize, this.minY + halfSize, this.minZ + halfSize, halfSize);
-  this.subdivided = true;
+  this.state = field.NodeState.SUBDIVIDED;
 }
 
+/**
+ * This function allows the caller to perform actions across regions of voxel space.
+ * The tree will be subdivided down to block level for the region specified, and the
+ * function XX called for each block.
+ * 
+ * @param func = function(minX, minY, minZ, blockSize, fieldArray)
+ */
+field.FieldNode.prototype.walkTree = function(minX, minY, minZ, maxX, maxY, maxZ, func) {
+}
+
+/*
 field.FieldNode.prototype.setValue(x, y, z, value) = function() {
   if (this.subdivided) {
     var child = this.whichChild(x, y, z);
@@ -65,49 +88,5 @@ field.FieldNode.prototype.setValue(x, y, z, value) = function() {
     this.value = value;
   }
 }
-
-// Returns value if uniform across node, else undefined.
-field.FieldNode.prototype.getUniformValue() = function() {
-  if (this.subdivided) {
-    var value = this.children[0].getUniformValue();
-    if (value !== undefined) {
-      for (var i = 1; i < 8; ++i) {
-        var subValue = this.children[i].getUniformValue();
-        if (subValue !== value) {
-          value = undefined;
-          break;
-        }
-      }
-    }
-    return value;
-  } else {
-    return this.value;
-  }
-}
-
-field.FieldNode.prototype.optimize() = function() {
-  if (this.subdivided) {
-    var value = this.getUniformValue();
-    if (value !== undefined) {
-      this.subdivided = false;
-      this.children = [];
-      this.value = value;
-    } else {
-      for (var i = 0; i < 8; ++i) {
-        // We can't optimize ourself, but maybe our children can.
-        this.children[i].optimize();
-      }
-    }
-  }
-}
-
-
-field.SparseField = function() {
-  
-  // SparseField is a sparse voxel octree. It only subdivides non-uniform
-  // regions. It has dimensions [-maxDim, maxDim-1] in each axis.
-  this.maxDim = 1 << 16;
-
-  this.rootNode = new field.FieldNode(-maxDim, -maxDim, -maxDim, maxDim * 2);
-};
+*/
 
