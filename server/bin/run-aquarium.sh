4606013a3944d311e28265224c740d344c827527
gman <gman@google.com>
2010-10-28 17:30:16 -0700

    update lg scripts for aquarium

--- server/bin/run-aquarium.sh
+++ server/bin/run-aquarium.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 SCRIPTDIR=`dirname $0`
-$SCRIPTDIR/run-chrome-across-machines.sh aquarium/aquarium.html
+$SCRIPTDIR/run-chrome-across-machines.sh aquarium/aquarium.html "100%25 HTML%2C, WebGL and JavaScript. 8 machines running Chrome using WebSockets, node.js and socket.io to stay in sync."
--- server/bin/run-chrome-across-machines.sh
+++ server/bin/run-chrome-across-machines.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 BASEURL=$1
+GMSG=$2
 GUSER=lg
 GADDRESS=lg1
 GPORT=3128
@@ -15,17 +16,17 @@ cd $CWD
 lg-sudo-bg ntpdate -u pool.ntp.org
 
 function doit {
-  ssh -x $GUSER@$1 "export DISPLAY=:0.0 ; ~/chrome/chrome-linux/chrome --kiosk --no-first-run --enable-webl --enable-accelerated-compositing http://$GDOMAIN/$GURL{\\\"net\\\":{\\\"id\\\":$2\,\\\"port\\\":$GPORT}}" &
+  ssh -x $GUSER@$1 "export DISPLAY=:0.0 ; ~/chrome/chrome-linux/chrome --kiosk --no-first-run --enable-webl --enable-accelerated-compositing http://$GDOMAIN/$GURL{\\\"net\\\":{\\\"id\\\":$2\,\\\"port\\\":$GPORT,\\\"msg\\\":\\\"$2\\\"}}" &
 }
 
 cd $SCRIPTDIR/../..
 node $SCRIPTDIR/../server.js --port $GPORT &
-doit lg1 0 
-doit lg2 1
-doit lg3 2
-doit lg4 3
-doit lg5 4
-doit lg6 -3
-doit lg7 -2
-doit lg8 -1
+doit lg1 0 "$1"
+doit lg2 1 ""
+doit lg3 2 ""
+doit lg4 3 ""
+doit lg5 4 ""
+doit lg6 -3 ""
+doit lg7 -2 ""
+doit lg8 -1 ""
 
