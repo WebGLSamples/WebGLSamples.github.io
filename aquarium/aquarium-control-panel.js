202188ed8207a2dfd529f244e7af239f92304972
gman <gman@google.com>
2010-12-01 14:41:28 -0800

    add timeout on control panel and adjust fog

--- aquarium/aquarium-control-panel.js
+++ aquarium/aquarium-control-panel.js
@@ -10,15 +10,29 @@ var math;                 // the math lib.
 var fast;                 // the fast math lib.
 var g_setSettingElements = [];
 var g_numSettingElements = {};
+var g_inactiveSeconds = 0;
+var g_inactiveTimeout = 30;
 
 var g_ui = [
   { obj: 'globals', label: 'Look Down - Up',               name: 'targetHeight',    value: 0,     max:  150 },
-  { obj: 'globals', label: 'Move Donw - Up',               name: 'eyeHeight',       value: 19,    max:  150 },
-  { obj: 'globals', label: 'Distance from Center of Tank', name: 'eyeRadius',       value: 60,    max:  200 },
+  { obj: 'globals', label: 'Move Down - Up',               name: 'eyeHeight',       value: 19,    max:  150 },
+  { obj: 'globals', label: 'Distance from Center of Tank', name: 'eyeRadius',       value: 60,    max:  150 },
   { obj: 'globals', label: 'Field Of View',                name: 'fieldOfView',     value: 85,    max:  120, min: 1},
   { obj: 'globals', label: 'Fog Edge Sharpness',           name: 'fogPower',        value: 14.5,  max:  50},
   { obj: 'globals', label: 'Fog Amount',                   name: 'fogMult',         value: 1.66,  max:  10},
 ];
+
+function markAsActive() {
+  g_inactiveSeconds = 0;
+}
+
+function checkActivity() {
+  ++g_inactiveSeconds;
+  if (g_inactiveSeconds >= g_inactiveTimeout) {
+    g_inactiveSeconds = 0;
+    advanceViewSettings();
+  }
+}
 
 /**
  * Initializes stuff.
@@ -29,9 +43,15 @@ function initialize() {
 
   initializeCommon();
 
+  setInterval(checkActivity, 1000);
+
   return true;
 }
 
+function updateUILocal(settings) {
+  updateUI(settings);
+  markAsActive();
+}
 
 $(function(){
   g.net.sync = true;
@@ -53,7 +73,7 @@ $(function(){
     setSettings({globals:{ fishSetting: 6}});
   });
 
-  g_syncManager = new tdl.sync.SyncManager(g, updateUI);
+  g_syncManager = new tdl.sync.SyncManager(g, updateUILocal);
 
   var div = document.getElementById("contentOuter");
   div.onmousedown = function() { return false; };
