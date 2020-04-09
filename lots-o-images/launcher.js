8e4f9e40e8c05944973b8d7247996992800473c2
Gregg Tavares <github@greggman.com>
2020-04-09 19:01:58 +0900

    update lots-o-images

---
+++ lots-o-images/launcher.js
@@ -0,0 +1,173 @@
+'use strict';
+
+/* global twgl, PerfHarness */
+
+function createElem(tag, attrs = {}) {
+  const elem = document.createElement(tag);
+  for (const [key, value] of Object.entries(attrs)) {
+    if (typeof value === 'object') {
+      for (const [k, v] of Object.entries(value)) {
+        try {
+        elem[key][k] = v;
+        } catch (e) {
+          debugger;  // eslint-disable-line no-debugger
+        }
+      }
+    } else if (elem[key] === undefined) {
+      elem.setAttribute(key, value);
+    } else {
+      elem[key] = value;
+    }
+  }
+  return elem;
+}
+
+function addElem(tag, parent, attrs = {}) {
+  const elem = createElem(tag, attrs);
+  parent.appendChild(elem);
+  return elem;
+}
+
+function noop() {
+}
+
+function addBool(parent, object, propertyName, callback = noop) {
+  const div = addElem('div', parent, {className:'fps'});
+  const id = `input-${performance.now()}-${Math.random() * 100000 | 0}`;
+  const input = addElem('input', div, {
+    type: 'checkbox',
+    id,
+    checked: object[propertyName],
+  });
+  addElem('label', div, {
+    for: id,
+    textContent: propertyName,
+  });
+  input.addEventListener('input', (e) => {
+    const v = input.checked;
+    object[propertyName] = v;
+    callback(v);
+  });
+}
+
+function initialize() {
+  const canvas = document.querySelector("#canvas");
+  const fpsElem = document.querySelector("#fps");
+  const afpsElem = document.querySelector("#afps");
+  const velElem = document.querySelector("#vel");
+  const cntElem = document.querySelector("#cnt");
+  const acntElem = document.querySelector("#acnt");
+  const targetFPSElem = document.querySelector("#targetFPS");
+
+  const query = new URLSearchParams(window.location.search);
+  function getQueryBool(id) {
+    const value = (query.get(id) || '').toLowerCase();
+    return value === 'true' || value === '1';
+  }
+
+  const settings =  {
+    alpha: getQueryBool('alpha'),
+    antialias: getQueryBool('antialias'),
+    preserveDrawingBuffer: getQueryBool('preserveDrawingBuffer'),
+    blend: getQueryBool('blend'),
+    update: true,
+  };
+
+  function reload() {
+    const s = new URLSearchParams();
+    s.set('alpha', settings.alpha);
+    s.set('antialias', settings.antialias);
+    s.set('preserveDrawingBuffer', settings.preserveDrawingBuffer);
+    s.set('blend', settings.blend);
+    window.location.search = s.toString();
+  }
+
+  const fpsContainer = document.querySelector('#fpsContainer');
+  addBool(fpsContainer, settings, 'alpha', reload);
+  addBool(fpsContainer, settings, 'preserveDrawingBuffer', reload);
+  addBool(fpsContainer, settings, 'antialias', reload);
+  addBool(fpsContainer, settings, 'blend');
+  addBool(fpsContainer, settings, 'update');
+
+  const targetFPS = 55;
+
+  const setTargetFPS = function(targetFPS) {
+    PerfHarness.setTargetFPS(targetFPS);
+    targetFPSElem.textContent = targetFPS;
+  };
+
+  $("#targetFPSSlider").slider({
+    max: 60,
+    min: 1,
+    value: targetFPS,
+    slide: function(event, ui) {
+      setTargetFPS(ui.value);
+    },
+  });
+  setTargetFPS(targetFPS);
+
+  document.querySelector("#title").textContent = document.querySelector("title").textContent;
+
+  const ctxOptions = {
+    alpha: settings.alpha,
+    antialias: settings.antialias,
+    preserveDrawingBuffer: settings.preserveDrawingBuffer,
+  };
+  const gl = canvas.getContext('webgl', ctxOptions);
+  if (!gl) {
+    return false;
+  }
+
+  const app = createApp(gl, settings);
+
+  const elapsedTimeHistory = new Array(10).fill(0);
+  let elapsedTimeTotal = 0;
+  let elapsedTimeHistoryCursor = 0;
+  let elapsedTimeAverage = 0;
+  let direction = 1;
+
+  function updateElapsedTimeHistory(elapsedTime) {
+    elapsedTimeTotal += elapsedTime - elapsedTimeHistory[elapsedTimeHistoryCursor];
+    elapsedTimeHistory[elapsedTimeHistoryCursor++] = elapsedTime;
+    elapsedTimeHistoryCursor %= elapsedTimeHistory.length;
+    elapsedTimeAverage = elapsedTimeTotal / elapsedTimeHistory.length;
+  }
+
+  const numFramesForStable = 1;
+  let lastDirection = 1;
+  let sameDirectionCount = 0;
+  let lastCount = 0;
+  let directionMark;
+  function trackCount(count) {
+    const direction = Math.sign(count - lastCount);
+    if (direction !== lastDirection) {
+      sameDirectionCount = 0;
+    } else {
+      ++sameDirectionCount;
+    }
+    lastDirection = direction;
+    if (sameDirectionCount < numFramesForStable) {
+      directionMark = "-";
+    } else {
+      directionMark = direction > 0 ? '^' : 'v';
+    }
+  }
+
+  function render(count, averageCount, elapsedTime, velocity) {
+    updateElapsedTimeHistory(elapsedTime);
+    trackCount(count);
+
+    // Update the FPS timer.
+    fpsElem.textContent = 1 / elapsedTime | 0;
+    afpsElem.textContent = 1 / elapsedTimeAverage | 0;
+
+    cntElem.textContent = `${count}${directionMark}`;
+
+    app.update(elapsedTime, count);
+    app.render();
+  }
+  PerfHarness.start(canvas, render, 60);
+  return true;
+}
+
+initialize();
---
+++ lots-o-images/lots-o-images-draw-elements.js
@@ -0,0 +1,174 @@
+function createApp(gl, settings) {
+  let g_numImages = 100;
+  const g_imageSize = 32;
+
+  const m4 = twgl.m4;
+  const vs = `
+  uniform vec2 dimensions;
+  uniform vec2 translation;
+  uniform vec4 colorMult;
+  attribute vec2 position;
+  attribute vec2 texCoord;
+  varying vec2 v_texCoord;
+  varying vec4 v_colorMult;
+  void main() {
+    v_texCoord = texCoord;
+    v_colorMult = colorMult;
+    gl_Position = vec4(
+      (((position + translation) / dimensions) * 2.0 - 1.0) * vec2(1, -1), 0, 1);
+  }
+  `;
+  const fs = `
+  precision mediump float;
+  varying vec2 v_texCoord;
+  varying vec4 v_colorMult;
+
+  uniform sampler2D diffuseSampler;
+
+  void main() {
+    gl_FragColor = texture2D(diffuseSampler, v_texCoord) * v_colorMult;
+  }
+  `;
+
+  const bufferInfo = twgl.createBufferInfoFromArrays(gl, {
+    position: { data: [0, 0, 32, 0, 0, 32, 32, 32], numComponents: 2, },
+    texCoord: [0, 0, 1, 0, 0, 1, 1, 1],
+    indices: [0, 1, 2, 2, 1, 3],
+  });
+  const texture = twgl.createTexture(gl, {src: '../google-io/2011/assets/google.png'});
+  const programInfo = twgl.createProgramInfo(gl, [vs, fs]);
+
+  function r(min, max) {
+    if (max === undefined) {
+      max = min;
+      min = 0;
+    }
+    return Math.random() * (max - min) + min;
+  }
+
+  // -- Setup Instances ----------------------------------
+  const instances = [];
+
+  function addInstance() {
+    const width = gl.canvas.width - g_imageSize;
+    const height = gl.canvas.height - g_imageSize;
+    const position = new Float32Array([r(width), r(height)]);
+    const colorMult = new Float32Array([r(1), r(1), r(1), r(1)]);
+    const vel = new Float32Array([1 * (r(1) < 0.5 ? -60 : 60), 1 * (r(1) < 0.5 ? -60 : 60)]);
+    const instance = {
+      position: position,
+      colorMult: colorMult,
+      vel: vel,
+      uniforms: {
+        translation: position,
+        colorMult: colorMult
+      },
+    };
+    instances.push(instance);
+  }
+
+  // pre-allocate a bunch of arrays
+  const v3t0 = new Float32Array(3);
+  const v3t1 = new Float32Array(3);
+  const v3t2 = new Float32Array(3);
+  const v3t3 = new Float32Array(3);
+  const m4t0 = new Float32Array(16);
+  const m4t1 = new Float32Array(16);
+  const m4t2 = new Float32Array(16);
+  const m4t3 = new Float32Array(16);
+  const zero4 = new Float32Array(4);
+  const one4 = new Float32Array([1,1,1,1]);
+  const dimensions = new Float32Array([1, 1]);
+
+  // uniforms.
+  const sharedUniforms = {
+      dimensions: dimensions,
+      diffuseSampler: texture,
+  };
+
+  let clock = 0.0;
+  function update(elapsedTime, _numImages) {
+    g_numImages = _numImages;
+    clock += elapsedTime;
+
+    while (instances.length < _numImages) {
+      addInstance();
+    }
+
+    twgl.resizeCanvasToDisplaySize(gl.canvas);
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    // --Update Instance Positions---------------------------------------
+    if (settings.update) {
+      const width = canvas.width - g_imageSize;
+      var height = canvas.height - g_imageSize;
+      var advance = elapsedTime;
+      for (var ii = 0; ii < g_numImages; ++ii) {
+        var instance = instances[ii];
+        instance.position[0] += advance * instance.vel[0];
+        instance.position[1] += advance * instance.vel[1];
+        if (instance.position[0] < 0) {
+          instance.position[0] = 0;
+          instance.vel[0] = -instance.vel[0];
+        } else if (instance.position[0] > width) {
+          instance.position[0] = width;
+          instance.vel[0] = -instance.vel[0];
+        }
+        if (instance.position[1]  < 0) {
+          instance.position[1]  = 0;
+          instance.vel[1] = -instance.vel[1];
+        } else if (instance.position[1]  > height) {
+          instance.position[1]  = height;
+          instance.vel[1] = -instance.vel[1];
+        }
+      }
+    }
+  }
+
+  function render() {
+    renderBegin();
+    renderScene();
+    renderEnd();
+  }
+
+  function renderBegin() {
+    // clear the screen.
+    gl.colorMask(true, true, true, true);
+    gl.clearColor(1, 1, 1, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    if (settings.blend) {
+      gl.enable(gl.BLEND);
+      gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+    }
+
+    // Pass in the dimensions of the canvas.
+    dimensions[0] = canvas.clientWidth;
+    dimensions[1] = canvas.clientHeight;
+  }
+
+  function renderScene() {
+    // -- Render Models ---------------------------------------
+    // We only need to render each model once
+    // as they contain all the instances.
+
+    gl.useProgram(programInfo.program);
+    twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
+    twgl.setUniforms(programInfo, sharedUniforms);
+
+    for (let ii = 0; ii < g_numImages; ++ii) {
+      const instance = instances[ii];
+      twgl.setUniforms(programInfo, instance.uniforms);
+      twgl.drawBufferInfo(gl, bufferInfo);
+    }
+  }
+
+  function renderEnd() {
+    // nothing to do.
+  }
+
+  return {
+    update: update,
+    render: render
+  };
+}
